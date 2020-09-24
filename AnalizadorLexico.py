dfb7e8e977281e2fd3cb82ad3b2547165ae5bf37
crabaqueropi <crabaqueropi@unal.edu.co>
2020-09-23 22:09:49 -0500

    Código Depurado
    Depuración de varias partes importantes del código

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -1,5 +1,6 @@
 import sys
 
+
 class Token:
     def __init__(self, tipo, lexema, fila, columna):
         self.tipo = tipo
@@ -12,9 +13,11 @@ class Token:
             return "<" + str(self.lexema) + "," + str(self.fila) + "," + str(self.columna) + ">"
         else:
             if self.tipo == "id":
-                return "<" + str(self.tipo) + "," + str(self.lexema) + "," + str(self.fila) + "," + str(self.columna) + ">"
+                return "<" + str(self.tipo) + "," + str(self.lexema) + "," + str(self.fila) + "," + str(
+                    self.columna) + ">"
             elif self.tipo == "tk_num" or self.tipo == "fid":
-                return "<" + str(self.tipo) + "," + str(self.lexema) + "," + str(self.fila) + "," + str(self.columna) + ">"
+                return "<" + str(self.tipo) + "," + str(self.lexema) + "," + str(self.fila) + "," + str(
+                    self.columna) + ">"
             else:
                 return "<" + str(self.tipo) + "," + str(self.fila) + "," + str(self.columna) + ">"
 
@@ -32,25 +35,23 @@ def delta(est, c):
         if c == ">":
             return (1, 0, None)
         elif c == "<":
-            return (4, 0, None)
-        elif c == "<":
-            return (4, 0, None)
+            return (2, 0, None)
         elif c == "+":
-            return (7, 0, None)
+            return (3, 0, None)
         elif c == "-":
-            return (11, 0, None)
+            return (4, 0, None)
         elif c == "*":
-            return (15, 0, None)
+            return (5, 0, None)
         elif c == "/":
-            return (18, 0, None)
+            return (6, 0, None)
         elif c == "%":
-            return (21, 0, None)
+            return (7, 0, None)
         elif c == ":":
-            return (24, 0, None)
+            return (8, 0, None)
         elif c == "=":
-            return (27, 0, None)
+            return (9, 0, None)
         elif c == "!":
-            return (30, 0, None)
+            return (10, 0, None)
         elif c == "{":
             return (0, 0, "tk_llave_izq")
         elif c == "}":
@@ -64,13 +65,13 @@ def delta(est, c):
         elif c == ";":
             return (0, 0, "tk_puntoycoma")
         elif c.isalpha():
-            return (38, 0, None)
+            return (11, 0, None)
         elif c == "@":
-            return (42, 0, None)
+            return (13, 0, None)
         elif c.isdigit():
-            return (45, 0, None)
+            return (15, 0, None)
         elif c == "#":
-            return (51, 0, None)
+            return (18, 0, None)
         elif c == " ":
             return (0, 0, "espacio")
         elif c == "\n":
@@ -80,198 +81,159 @@ def delta(est, c):
         elif c == "\r":
             return (0, 0, "retorno")
         else:
-            return (29, 0, "Error léxico")
+            return (0, 0, "Error léxico")
 
     elif est == 1:
         if c == "=":
             return (0, 0, "tk_mayor_igual")
         else:
             return (0, 1, "tk_mayor")
-    # elif est == 2:
-    #     return -1
-    # elif est == 3:
-    #     return -1
-    elif est == 4:
+    elif est == 2:
         if c == "=":
             return (0, 0, "tk_menor_igual")
         else:
             return (0, 1, "tk_menor")
-    # elif est == 5:
-    #     return -1
-    # elif est == 6:
-    #     return -1
-    elif est == 7:
+    elif est == 3:
         if c == "=":
             return (0, 0, "tk_sum_asig")
         elif c == "+":
             return (0, 0, "tk_incremento")
         else:
             return (0, 1, "tk_mas")
-    # elif est == 8:
-    #     return -1
-    # elif est == 9:
-    #     return -1
-    # elif est == 10:
-    #     return -1
-    elif est == 11:
+    elif est == 4:
         if c == "=":
             return (0, 0, "tk_res_asig")
-        elif c == "+":
+        elif c == "-":
             return (0, 0, "tk_decremento")
         else:
             return (0, 1, "tk_menos")
-    # elif est == 12:
-    #     return -1
-    # elif est == 13:
-    #     return -1
-    # elif est == 14:
-    #     return -1
-    elif est == 15:
+    elif est == 5:
         if c == "=":
             return (0, 0, "tk_mul_asig")
         else:
             return (0, 1, "tk_mul")
-    # elif est == 16:
-    #     return -1
-    # elif est == 17:
-    #     return -1
-    elif est == 18:
+    elif est == 6:
         if c == "=":
             return (0, 0, "tk_div_asig")
         else:
             return (0, 1, "tk_div")
-    # elif est == 19:
-    #     return -1
-    # elif est == 20:
-    #     return -1
-    elif est == 21:
+    elif est == 7:
         if c == "=":
             return (0, 0, "tk_mod_asig")
         else:
             return (0, 1, "tk_mod")
-    # elif est == 22:
-    #     return -1
-    # elif est == 23:
-    #     return -1
-    elif est == 24:
+    elif est == 8:
         if c == "=":
             return (0, 0, "tk_asignacion")
         else:
             return (0, 1, "tk_dospuntos")
-    # elif est == 25:
-    #     return -1
-    # elif est == 26:
-    #     return -1
-    elif est == 27:
+    elif est == 9:
         if c == "=":
             return (0, 0, "tk_igualdad")
         else:
             return (0, 1, "Error léxico")
-    # elif est == 28:
-    #     return -1
-    # elif est == 29:
-    #     pass
-    elif est == 30:
+    elif est == 10:
         if c == "=":
             return (0, 0, "tk_diferente")
         else:
             return (0, 1, "Error léxico")
-    # elif est == 31:
-    #     return -1
-    # elif est == 32:
-    #     return -1
-    # elif est == 33:
-    #     return -1
-    # elif est == 34:
-    #     return -1
-    # elif est == 35:
-    #     return -1
-    # elif est == 36:
-    #     return -1
-    # elif est == 37:
-    #     return -1
-    elif est == 38:
+    elif est == 11:
         if c.isalpha():
-            return (38, 0, None)
+            return (11, 0, None)
         elif c.isdigit():
-            return (40, 0, None)
+            return (12, 0, None)
         else:
             return (0, 1, "REVISAR")
-    # elif est == 39:
-    #     return -1
-    elif est == 40:
+    elif est == 12:
         if c.isalnum():
-            return (40, 0, None)
+            return (12, 0, None)
         else:
             return (0, 1, "id")
-    # elif est == 41:
-    #     return -1
-    elif est == 42:
+    elif est == 13:
         if c.isalpha():
-            return (43, 0, None)
+            return (14, 0, None)
         else:
             return (0, 1, "Error léxico")
-    elif est == 43:
+    elif est == 14:
         if c.isalnum():
-            return (43, 0, None)
+            return (14, 0, None)
         else:
             return (0, 1, "fid")
-    # elif est == 44:
-    #     return -1
-    elif est == 45:
+    elif est == 15:
         if c.isdigit():
-            return (45, 0, None)
+            return (15, 0, None)
         elif c == ".":
-            return (46, 0, None)
+            return (16, 0, None)
         else:
             return (0, 1, "tk_num")
-    elif est == 46:
+    elif est == 16:
         if c.isdigit():
-            return (49, 0, None)
+            return (17, 0, None)
         else:
             return (0, 2, "tk_num")
-    # elif est == 47:
-    #     return -1
-    # elif est == 48:
-    #     return -1
-    elif est == 49:
+    elif est == 17:
         if c.isdigit():
-            return (49, 0, None)
+            return (17, 0, None)
         else:
             return (0, 1, "tk_num")
-    # elif est == 50:
-    #     return -1
-    elif est == 51:
+    elif est == 18:
         if c == "\n":
             return (0, 0, "comentario")
         else:
-            return (51, 0, None)
-    # elif est == 52:
-    #     pass
-    # elif est == 53:
-    #     if c == " ":
-    #         return (53, 0, None)
-    #     else:
-    #         return (0, 1, "espacios")
-    # elif est == 54:
-    #     pass
-    # elif est == 55:
-    #     return -1
+            return (18, 0, None)
     else:
         print("errorEnEstados")
 
 
+def definirToken(devolver, tipoToken, devueltos, fila, columna, buffer):
+    if devolver > 0:
+        endLoc = len(buffer)
+        startLoc = endLoc - devolver
+        devueltos += buffer[startLoc: endLoc]
+        nuevoBuffer = buffer[0: startLoc]
+        columna -= devolver
+    else:
+        nuevoBuffer = buffer
+
+    buffer = ""
+    if tipoToken == "saltoLinea" or tipoToken == "comentario":
+        fila += 1
+        columna = 1
+    elif tipoToken == "espacio" or tipoToken == "retorno":
+        pass  # ya había sumado 1 antes en la columna
+    elif tipoToken == "tabulacion":
+        columna += 3  # ya había sumado 1 antes
+    else:
+        if nuevoBuffer in palabrasReservadas:
+            token = Token(nuevoBuffer, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
+            print(token)
+        else:
+            if tipoToken == "REVISAR":
+                token = Token("id", nuevoBuffer, fila, (columna - len(nuevoBuffer)))
+                print(token)
+            elif tipoToken == "tk_num" or tipoToken == "fid":
+                token = Token(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
+                print(token)
+            else:
+                token = Token(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
+                print(token)
+
+    return devueltos, fila, columna, buffer
+
 def analizarLexico(codigo):
-    codigo += " "
+    codigo += " "  # para que detecte el último lexema del código
     fila = 1
     columna = 1
     estadoAux = 0
     buffer = ""
     devueltos = ""
     bucleInterno = True
+
     for caracter in codigo:
+
         if not bucleInterno:
             break
+
         buffer += caracter
         columna += 1
         estadoAux, devolver, tipoToken = delta(estadoAux, caracter)
@@ -279,44 +241,10 @@ def analizarLexico(codigo):
         if tipoToken is not None:
 
             if tipoToken == "Error léxico":
-                print("Error léxico(línea:" + str(fila) + ",posición:" + str(columna-1-devolver) + ")")
+                print("Error léxico(línea:" + str(fila) + ",posición:" + str(columna - 1 - devolver) + ")")
                 break
 
-            if devolver > 0:
-                endLoc = len(buffer)
-                startLoc = endLoc - devolver
-                devueltos += buffer[startLoc: endLoc]
-                nuevoBuffer = buffer[0: startLoc]
-                columna -= devolver
-            else:
-                nuevoBuffer = buffer
-
-            buffer = ""
-            if tipoToken == "saltoLinea" or tipoToken == "comentario":
-                fila += 1
-                columna = 1
-            elif tipoToken == "espacio" or tipoToken == "retorno":
-                pass #ya había sumado 1 antes en la columna
-            elif tipoToken == "tabulacion":
-                columna += 3 #ya había sumado 1 antes
-            else:
-                if nuevoBuffer in palabrasReservadas:
-                    token = Token(nuevoBuffer, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                    # print(nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                    print(token)
-                else:
-                    if tipoToken == "REVISAR":
-                        #print("id", nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                        token = Token("id", nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                        print(token)
-                    elif tipoToken == "tk_num" or tipoToken == "fid":
-                        #print(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                        token = Token(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                        print(token)
-                    else:
-                        #print(tipoToken, fila, (columna - len(nuevoBuffer)))
-                        token = Token(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                        print(token)
+            devueltos, fila, columna, buffer = definirToken(devolver, tipoToken, devueltos, fila, columna, buffer)
 
             for caracterDev in devueltos:
                 buffer += caracterDev
@@ -326,49 +254,16 @@ def analizarLexico(codigo):
                 if tipoToken is not None:
 
                     if tipoToken == "Error léxico":
-                        print("Error léxico(línea:" + str(fila) + ",posición:" + str(columna-1-devolver) + ")")
+                        print("Error léxico(línea:" + str(fila) + ",posición:" + str(columna - 1 - devolver) + ")")
                         bucleInterno = False
                         break
 
-                    if devolver > 0:
-                        endLoc = len(buffer)
-                        startLoc = endLoc - devolver
-                        devueltos += buffer[startLoc: endLoc]
-                        nuevoBuffer = buffer[0: startLoc]
-                        columna -= devolver
-                    else:
-                        nuevoBuffer = buffer
+                    devueltos, fila, columna, buffer = definirToken(devolver, tipoToken, devueltos, fila, columna, buffer)
 
-                    buffer = ""
-                    if tipoToken == "saltoLinea" or tipoToken == "comentario":
-                        fila += 1
-                        columna = 1
-                    elif tipoToken == "espacio" or tipoToken == "retorno":
-                        pass # ya había sumado 1 antes en la columna
-                    elif tipoToken == "tabulacion":
-                        columna += 3  # ya había sumado 1 antes
-                    else:
-                        if nuevoBuffer in palabrasReservadas:
-                            token = Token(nuevoBuffer, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                            # print(nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                            print(token)
-                        else:
-                            if tipoToken == "REVISAR":
-                                # print("id", nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                                token = Token("id", nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                                print(token)
-                            elif tipoToken == "tk_num" or tipoToken == "fid":
-                                # print(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                                token = Token(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                                print(token)
-                            else:
-                                # print(tipoToken, fila, (columna - len(nuevoBuffer)))
-                                token = Token(tipoToken, nuevoBuffer, fila, (columna - len(nuevoBuffer)))
-                                print(token)
             devueltos = ""
 
 
-cod = '''var z:num;
+cod1 = '''var z:num;
 z := 0;
 while (z < 10)
   {
@@ -378,7 +273,7 @@ while (z < 10)
 end
 # salida: 1 2 3 4 5 6 7 8 9 10'''
 
-cod2 ='''## función min(x, y)
+cod2 = '''## función min(x, y)
 function @min:num (x:num, y:num)
   {
   when ((x < y) == true) do return x;
@@ -406,14 +301,13 @@ end'''
 
 cod3 = "2.5598055while3!=88¬56.a"
 
-cod4 = '''asdasd 1512=o'''
+cod4 = "!1"
+
+analizarLexico(cod2)
 
 # lineasSeparadas = sys.stdin.readlines()
 # lineas = ""
 # for linea in lineasSeparadas:
 #     lineas += linea
 #
-# #analizarLexico(lineas)
-
-analizarLexico(cod4)
-
+# analizarLexico(lineas)
